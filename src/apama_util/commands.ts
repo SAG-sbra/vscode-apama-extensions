45ec4d69c12821411533b7ec1c0f05adade74b28
Bradshaw <sbra@softwareag.com>
2020-04-01 16:08:21 +0100

    Changing engine_send to avoid ApamaAsyncRunner

--- src/apama_util/commands.ts
+++ src/apama_util/commands.ts
@@ -1,19 +1,18 @@
 import { ApamaRunner, ApamaAsyncRunner } from '../apama_util/apamarunner';
 import * as vscode from 'vscode';
 import { ApamaEnvironment } from '../apama_util/apamaenvironment';
+import { ChildProcess, spawn } from 'child_process';
 import { Writable } from 'stream';
 
 export class ApamaCommandProvider{
     private injectCmd: ApamaRunner;
     private sendCmd: ApamaRunner;
-    private sendCmdAsync: ApamaAsyncRunner;
     private deleteCmd: ApamaRunner;
     private apamaEnv: ApamaEnvironment;
 
     public constructor(private logger: vscode.OutputChannel, apamaEnv: ApamaEnvironment, private context: vscode.ExtensionContext) {
         this.injectCmd = new ApamaRunner("engine_inject", apamaEnv.getInjectCmdline(), logger);
         this.sendCmd = new ApamaRunner("engine_send", apamaEnv.getSendCmdLine(), logger);
-        this.sendCmdAsync = new ApamaAsyncRunner("engine_send", apamaEnv.getSendCmdLine(), logger);
         this.deleteCmd = new ApamaRunner("engine_delete", apamaEnv.getDeleteCmdLine(), logger);
         this.apamaEnv = apamaEnv;
         this.registerCommands();
@@ -29,7 +28,10 @@ export class ApamaCommandProvider{
                 // engine_inject command
                 //
 		        vscode.commands.registerCommand('extension.apama.engine_inject', (monFile) => {
-                    this.injectCmd.run('.',['-p', port.toString()].concat(monFile.fsPath))
+                    if(monFile !== undefined){
+                        this.injectCmd.run('.',['-p', port.toString()].concat(monFile.fsPath))
+                    }
+                    // TODO?: add option to specify mon file name to inject in command palette 
                 }),
                 //
                 // engine_send command
@@ -48,13 +50,17 @@ export class ApamaCommandProvider{
 						    placeHolder: "Specify event to send"
                         });
 						if (userInput !== undefined) {
-                            // Specify engine_send command with NO evt files  
-                            const child = this.sendCmdAsync.start(['-p',port.toString()], true, true);
+                            // Specify engine_send command with NO evt files (but specify port) 
+                            const childProcess = spawn(this.apamaEnv.getSendCmdLine()+' -p '+port.toString(), {
+                                shell: true,
+                                stdio: ['pipe', 'pipe', 'pipe']
+                              });
                             // When no evt files are specified in engine_send command 
                             // the correlator reads user-specified events from stdin 
                             // (see 'Sending events to correlators' in 'Deploying and Managing Apama Applications' p.169).
                             //  => write userInput to stdin of child process:
-                            await this.streamWrite(child.stdin, userInput+"\n");
+                            this.streamWrite(childProcess.stdin, userInput+"\n");
+                            await this.onExit(childProcess);
                             this.logger.appendLine('engine_send '+userInput);
                         }
                     }
@@ -94,6 +100,21 @@ export class ApamaCommandProvider{
             };
             stream.write(chunk, encoding, callback);
           });
+      }
+
+      onExit(childProcess: ChildProcess): Promise<void> {
+        return new Promise((resolve, reject) => {
+          childProcess.once('exit', (code: number, signal: string) => {
+            if (code === 0) {
+              resolve(undefined);
+            } else {
+              reject(new Error('Exit with error code: '+code));
+            }
+          });
+          childProcess.once('error', (err: Error) => {
+            reject(err);
+          });
+        });
       }
 
 }
